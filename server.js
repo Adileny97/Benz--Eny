1d83614fc42dc7025eea7d0c4c0794dbe952ef93
Adel <adilcannes11@gmail.com>
2026-06-01 02:55:29 +0200

    Integrate morgan for request logging
    Added morgan middleware for logging HTTP requests.

--- server.js
+++ server.js
@@ -48,4 +48,7 @@ app.post('/contact', (req, res) => {
 // Start server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    const morgan = require('morgan');
+app.use(morgan('dev'));
+
 });
